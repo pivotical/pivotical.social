8b56eebd54e684bf8b0dc7fba564e87980ea6ac2
Roshni V R <vrroshni2001@gmail.com>
2023-08-28 13:07:28 +0530

    added navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
+import ResponsiveAppBar from './components/NavBar'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import ThemeRegistry from './theme/ThemeRegistry'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ThemeRegistry >
+        <body>
+          <ResponsiveAppBar />
+          {children}</body>
+      </ThemeRegistry>
     </html>
   )
 }
---
+++ src/app/theme/ThemeRegistry.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Roboto } from "next/font/google";
+import NextAppDirEmotionCacheProvider from "./EmotionCache";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
+
+const themeOptions = {
+  typography: {
+    fontSize: 12,
+    fontFamily: roboto.style.fontFamily,
+  },
+  palette: {
+    background: {
+      // pink
+      default: "#ffffff",
+    },
+    primary: {
+      main: "#1976d2",
+    },
+    text: {
+      primary: "#300000",
+    },
+  },
+};
+
+const theme = createTheme(themeOptions);
+
+export default function ThemeRegistry({ children }) {
+  return (
+    <NextAppDirEmotionCacheProvider options={{ key: "mui" }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </NextAppDirEmotionCacheProvider>
+  );
+}
