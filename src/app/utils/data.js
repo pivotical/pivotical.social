1060e7250e46179f7ff6c4e647563e38f70045fa
Roshni V R <vrroshni2001@gmail.com>
2023-09-02 19:16:29 +0530

    added footer section

--- src/app/utils/data.js
+++ src/app/utils/data.js
@@ -16,3 +16,69 @@ export const links = [
         link: '#contact'
     },
 ]
+export const footerlinks = [
+    {
+        id: 1,
+        section: 'Resources',
+        sectionlinks:[
+            {
+                id: 112,
+                href: 'footerlinkssss',
+                name: 'footerlinkssss'
+            },
+            {
+                id: 113,
+                href: 'about',
+                name: 'footerlinkssss'
+            },
+            {
+                id: 134,
+                href: 'about',
+                name: 'footerlinkssss'
+            },
+        ]
+    },
+    {
+        id: 1,
+        section: 'SocialMediaaaa',
+        sectionlinks:[
+            {
+                id: 12345,
+                href: 'footerlinkssss',
+                name: 'footerlinkssss'
+            },
+            {
+                id: 5678,
+                href: 'about',
+                name: 'footerlinkssss'
+            },
+            {
+                id: 345678,
+                href: 'about',
+                name: 'footerlinkssss'
+            },
+        ]
+    },
+    {
+        id: 1567,
+        section: 'home',
+        sectionlinks:[
+            {
+                id: 1612,
+                href: 'footerlinkssss',
+                name: 'footerlinkssss'
+            },
+            {
+                id: 1613,
+                href: 'about',
+                name: 'footerlinkssss'
+            },
+            {
+                id: 16634,
+                href: 'about',
+                name: 'footerlinkssss'
+            },
+        ]
+    },
+   
+]
